2cf04c8896b88cc4f8c2df48497f15a582796327
LeManagerDave <132759771+LeManagerDave@users.noreply.github.com>
2023-06-10 10:59:49 +0000

    Create put "update dishes and drinks"

--- dish/src/main/java/ch/fhnw/dish/business/service/MenuService.java
+++ dish/src/main/java/ch/fhnw/dish/business/service/MenuService.java
@@ -59,6 +59,24 @@ public class MenuService {
         throw new Exception("Invalid drink name ");
     }
 
+    public Dish updateDish(Long id, Dish updatedDish) {
+        Dish dish = findDishById(id);
+        dish.setDishName(updatedDish.getDishName());
+        dish.setDescription(updatedDish.getDescription());
+        dish.setPrice(updatedDish.getPrice());
+        // Update any other properties as needed
+        return dishRepository.save(dish);
+    }
+
+    public Drink updateDrink(Long id, Drink updatedDrink) {
+        Drink drink = findDrinkById(id);
+        drink.setDrinkName(updatedDrink.getDrinkName());
+        drink.setDescription(updatedDrink.getDescription());
+        drink.setPrice(updatedDrink.getPrice());
+        // Update any other properties as needed
+        return drinkRepository.save(drink);
+    }
+
     // Business Logic to get current offer according to the location of the user requesting the menu
     private String getCurrentOffer(String location) {
         String currentOffer = "No special offer";
